771c8e4adbc7cb7e8e4431b6c93f642348b8990b
yamada <yamada@aptpod.co.jp>
2019-12-02 17:26:18 +0900

    fix: users table ddl

--- db/ddl/users.sql
+++ db/ddl/users.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS edge_enable (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY NOT NULL,
-    name VARCHAR(36) NOT NULL,
+    first_name VARCHAR(36) NOT NULL,
+    family_name VARCHAR(36) NOT NULL,
     age INTEGER NOT NULL,
     birthday TIMESTAMP,
     address VARCHAR(128),
